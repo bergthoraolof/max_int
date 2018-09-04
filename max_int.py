2221f4b4fa9e324e6d9cf1947abe3f1ae28a0f45
bergthoraolof <35221015+bergthoraolof@users.noreply.github.com>
2018-09-04 10:55:15 +0000

    Update max_int.py

--- max_int.py
+++ max_int.py
@@ -1,10 +1,11 @@
 #Finding the maximum positive integer input by a user
 num_int = int(input("Input a number: ")) #Input an integer
 #The input has to be a positive integer for the loop to run
+max_int = 0         #Set max_int value to 0 to define the variable
 while num_int > 0:      #See whether the integer is positive
-    max_int = 0         #Set max_int value to 0 to define the variable
     if num_int > max_int:    #Find the maximum positive integer
         max_int = num_int
-     num_int = int(input("Input a number: "))      #Input an integer
+    num_int = int(input("Input a number: "))      #Input an integer
    
 print("The maximum is", max_int)  #print maximum  
+
